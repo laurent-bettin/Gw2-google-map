94db390ff15e77af365c49db19c16980924ec98b
laurent-bettin <b.rhaze@gmail.com>
2013-11-22 14:32:30 +0100

    Using deferred

--- js/MapNames.js
+++ js/MapNames.js
@@ -1,40 +1,41 @@
 MapNames = function(map, maxZoom) {
-    this._map = map;
-    this._names = null;
-    this._maxZoom = maxZoom;
+    this.map = map;
+    this.infosRegion = [];
+    this.maxZoom = maxZoom;
     this.infosWindows = [];
-}
+};
 
-MapNames.prototype.display_infowindow = function() {
-    var namesLength = this._names.length;
-    for (var i=0; i<namesLength; i++) {
+MapNames.prototype.set_infos_region = function(infosRegion) {
+    this.infosRegion = infosRegion;
+};
+
+MapNames.prototype.create_infowindow = function() {
+    var lenIr = this.infosRegion.length;
+    for (var i=0; i<lenIr; i++) {
         // overlay = new nameOverlay(mapBound, data.regions[region].name, map);
-        var mapPoint = new google.maps.Point(this._names[i][1][0], this._names[i][1][1]),
+        var mapPoint = new google.maps.Point(this.infosRegion[i][1][0], this.infosRegion[i][1][1]),
             infowindow = new google.maps.InfoWindow({
-                content: '<b>'+this._names[i][0]+'</b>',
-                position: fromPointToLatLng(mapPoint, this._maxZoom)
+                content: '<b>'+this.infosRegion[i][0]+'</b>',
+                position: fromPointToLatLng(mapPoint, this.maxZoom)
             });
         this.infosWindows.push(infowindow);
-        infowindow.open(this._map);
-    }
-}
-
-MapNames.prototype.display_regions = function(names) {
-    var zoom_level = this._map.getZoom();
-    this._names = names;
-    if(zoom_level === 2) {
-        this.display_infowindow();
     }
 };
 
 MapNames.prototype.update_display = function() {
-    var len = this.infosWindows.length;
-    var zoom_level = this._map.getZoom();
+    var lenIw = this.infosWindows.length;
+    var lenIr = this.infosRegion.length;
+    if(!lenIw) {
+        this.create_infowindow();
+    }
+    var zoom_level = this.map.getZoom();
     if(zoom_level === 2) {
-        this.display_infowindow();
+        for(var i=0; i<lenIr; i++) {
+            this.infosWindows[i].open(this.map);
+        }
     }else{
-        for(var i=0; i<len; i++) {
+        for(var i=0; i<lenIr; i++) {
             this.infosWindows[i].close();
         }
     }
-}
+};
--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+function delay(a){var b=(new Date).getTime()+a;while(b>(new Date).getTime());}
 var front = {};
 /*           85
  *     ---------------
@@ -23,106 +24,93 @@ function fromPointToLatLng(point, max_zoom) {
 (function (context) {
     "use strict";
 
-    var mapFloorUrl = "https://api.guildwars2.com/v1/map_floor.json?continent_id=1&floor=2";
-    var mapNamesRequest = $.getJSON("https://api.guildwars2.com/v1/map_names.json");
-    var mapFloorRequest = $.getJSON(mapFloorUrl);
     var datas_regions = [];
     var explorablesZones = [];
     var explorablesZonesId = [];
+    var mapNamesManager;
 
 
-    var map, southwest, northeast, northwest, southeast;
+    var southwest, northeast;
 
     southwest = fromPointToLatLng(new google.maps.Point(0, 32768), 7);
     northeast = fromPointToLatLng(new google.maps.Point(32768, 0), 7);
     // var map_bounds = new google.maps.LatLngBounds(southwest, northeast);
 
-    var Gw2MapOptions = {
-        tileSize: new google.maps.Size(256, 256),
-        maxZoom: 7,
-        minZoom: 2,
-        name: "GW2WorldMap",
-        getTileUrl: function(coord, zoom) {
-            var normalizedCoord = ui.getNormalizedCoord(coord, zoom);
-            if (!normalizedCoord) {
-                return null;
+    var overlay;
+
+    var dataLoader = {
+        d: $.Deferred(),
+        result: [],
+        setters: [],
+        resolveDeferred: function() {
+            this.d.resolve(this.result, this.setters)
+        },
+        requestDatas: function(url, key) {
+            var request = $.getJSON(url);
+            return request;
+        },
+        init: function(urls, setters) {
+            this.setters = setters;
+            var _this = this,
+                len = urls.length;
+
+            for(var i=0; i<len; i++) {
+                this.result.push(this.requestDatas(urls[i]));
             }
-            // https://tiles.guildwars2.com/1/1/zoom/{x}/{y}.jpg
-            return "https://tiles.guildwars2.com/1/1/"+zoom+"/"+normalizedCoord.x+"/"+normalizedCoord.y+".jpg";
-            // return url;
+
+            // http://stackoverflow.com/questions/6538470/jquery-deferred-waiting-for-multiple-ajax-requests-to-finish
+            $.when.apply($, this.result)
+                .done($.proxy(_this.resolveDeferred, _this));
+
+            return this.d.promise();
         }
     };
 
-    nameOverlay.prototype = new google.maps.OverlayView();
+    // var dataLoader = {
+    //     d: $.Deferred(),
+    //     datas: {},
+    //     resolveDeferred: function() {
+    //         this.d.resolve(this.datas)
+    //     },
+    //     setDatas: function(datas, key) {
+    //         this.datas[key['key']] = datas[0];
+    //     },
+    //     requestDatas: function(url, key) {
+    //         var _this = this;
+    //         var request = $.getJSON(url);
+    //         $.when(request, {'key': key})
+    //             .done($.proxy(_this.setDatas, _this));
+    //         return request;
+    //     },
+    //     init: function(keyUrl) {
+    //         var _this = this,
+    //             result = [];
 
-    // https://developers.google.com/maps/documentation/javascript/examples/overlay-simple
-    function nameOverlay (bounds, text, map) {
-        // Initialize all properties.
-        this.bounds_ = bounds;
-        this.text_ = text;
-        this.map_ = map;
+    //         for(var r in keyUrl) {
+    //             result.push(this.requestDatas(keyUrl[r], r));
+    //         }
 
-        // Define a property to hold the image's div. We'll
-        // actually create this div upon receipt of the onAdd()
-        // method so we'll leave it null for now.
-        this.div_ = null;
+    //         // http://stackoverflow.com/questions/6538470/jquery-deferred-waiting-for-multiple-ajax-requests-to-finish
+    //         $.when.apply($, result)
+    //             .done($.proxy(_this.resolveDeferred, _this));
 
-        // Explicitly call setMap on this overlay.
-        this.setMap(map);
-    };
+    //         return this.d.promise();
+    //     }
+    // };
 
-    nameOverlay.prototype.onAdd = function() {
-        var div = document.createElement('div');
-        div.style.borderStyle = 'none';
-        div.style.borderWidth = '0px';
-        div.style.position = 'absolute';
-
-        //creation d'un paragraphe
-        var txt = document.createTextNode(this.text_);
-        var paragraph = document.createElement('p');
-        paragraph.appendChild(txt);
-        div.appendChild(paragraph)
-
-        this.div_ = div;
-
-        // Add the element to the "overlayLayer" pane.
-        var panes = this.getPanes();
-        panes.overlayLayer.appendChild(div);
-    };
-
-    nameOverlay.prototype.draw = function() {
-
-        // We use the south-west and north-east
-        // coordinates of the overlay to peg it to the correct position and size.
-        // To do this, we need to retrieve the projection from the overlay.
-        var overlayProjection = this.getProjection();
-
-        // Retrieve the south-west and north-east coordinates of this overlay
-        // in LatLngs and convert them to pixel coordinates.
-        // We'll use these coordinates to resize the div.
-        var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
-        var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
-
-        // Resize the image's div to fit the indicated dimensions.
-        var div = this.div_;
-        div.style.left = sw.x + 'px';
-        div.style.top = ne.y + 'px';
-        div.style.width = (ne.x - sw.x) + 'px';
-        div.style.height = (sw.y - ne.y) + 'px';
-
-    };
-
-    nameOverlay.prototype.onRemove = function() {
-        this.div_.parentNode.removeChild(this.div_);
-        this.div_ = null;
-    };
-
-    var overlay;
-
-    var ui = {
-        initialize: function () {
-
-            var GW2WorldMap = new google.maps.ImageMapType(Gw2MapOptions),
+    var gmap = {
+        Gw2MapUiOptions: {
+            tileSize: new google.maps.Size(256, 256),
+            maxZoom: 7,
+            minZoom: 2,
+            name: "GW2WorldMap"
+        },
+        getMaxZoom: function() {
+            return this.Gw2MapUiOptions.maxZoom;
+        },
+        init: function() {
+            this.Gw2MapUiOptions.getTileUrl = this.getTileUrl;
+            var GW2WorldMap = new google.maps.ImageMapType(this.Gw2MapUiOptions),
                 GW2WorldMapOpts = {
                     center : new google.maps.LatLng(0, 0),
                     zoom : 2,
@@ -134,101 +122,18 @@ function fromPointToLatLng(point, max_zoom) {
                 };
 
 
-            map = new google.maps.Map(document.getElementById("GW2map"), GW2WorldMapOpts);
+            var map = new google.maps.Map(document.getElementById("GW2map"), GW2WorldMapOpts);
             map.mapTypes.set('custom', GW2WorldMap);
 
-            google.maps.event.addListener(map, 'zoom_changed', function(ev) {
-                mapNamesManager.update_display();
-            });
-
-            // map.overlayMapTypes.insertAt(0, new CoordMapType(new google.maps.Size(256, 256)));
-            var p = new google.maps.Point(12839.8, 12483.8)
-            var myLatLng = fromPointToLatLng(p, 7);
-            // var marker = ui.addMarker(myLatLng);
-            // ui.addMarker(Snowden_Drifts_Southwest);
-            // ui.addMarker(Snowden_Drifts_Southeast);
-            // ui.addMarker(Snowden_Drifts_Northwest);
-            // ui.addMarker(new google.maps.LatLng(-85, 180));
-
-            var mapNamesManager = new MapNames(map, Gw2MapOptions.maxZoom);
-            var mapNamesDeferred = $.Deferred();
-            mapNamesDeferred.done($.proxy(mapNamesManager.display_regions, mapNamesManager));
-
-            var mapDrawingDeferred = $.Deferred();
-            mapDrawingDeferred.done(ui.draw_explorables_zones);
-
-            $.when(mapNamesRequest).done(ui.get_explorables_zones_id)
-            $.when(mapFloorRequest).done([ui.get_datas_regions, ui.get_explorables_zones]);
-
-            mapNamesDeferred.resolve(datas_regions);
-            mapDrawingDeferred.resolve(explorablesZones, map)
-
-
+            return map;
         },
-        get_explorables_zones_id: function(mapNamesRequestResult) {
-            var mapNamesDatas = mapNamesRequestResult;
-            for (var i in mapNamesDatas) {
-                explorablesZonesId.push(mapNamesDatas[i]["id"]);
-            };
-        },
-        get_datas_regions: function(mapFloorRequestResult) {
-            var mapFloorDatas = mapFloorRequestResult.regions;
-            for (var r in mapFloorDatas) {
-                // overlay = new nameOverlay(map_bounds, data1.mapFloorDatas[region].name, map);
-                datas_regions.push([mapFloorDatas[r]['name'], mapFloorDatas[r]['label_coord']]);
+        getTileUrl: function(coord, zoom) {
+            var normalizedCoord = gmap.getNormalizedCoord(coord, zoom);
+            if (!normalizedCoord) {
+                return null;
             }
-        },
-        get_explorables_zones: function(mapFloorRequestResult) {
-            var mapFloorDatas = mapFloorRequestResult.regions;
-            for (var r in mapFloorDatas) {
-                for(var mapId in mapFloorDatas[r]['maps']) {
-                    //City are not present in map_names.json but we could find them in map_floor.json
-                    //because minLvl is 0 and maxLvl 80
-                    var minLvl = mapFloorDatas[r]['maps'][mapId]['min_level'];
-                    var maxLvl = mapFloorDatas[r]['maps'][mapId]['max_level'];
-                    if(explorablesZonesId.indexOf(mapId) != -1 || (minLvl == 0 && maxLvl == 80)) {
-                        explorablesZones.push([mapFloorDatas[r]['maps'][mapId]['name'], mapFloorDatas[r]['maps'][mapId]['continent_rect']])
-                    }
-                }
-            }
-        },
-        draw_explorables_zones: function(map_datas, map) {
-            var len = map_datas.length;
-            // https://google-developers.appspot.com/maps/documentation/javascript/examples/polygon-simple
-            for(var i=0; i<len; i++) {
-                var mapRect = map_datas[i][1];
-
-                // [[17664, 11264],[21760, 13312]] -> [northwest, southeast]
-                var mapRectNorthwest = fromPointToLatLng(new google.maps.Point(mapRect[0][0], mapRect[0][1]), 7),
-                    mapRectNortheast = fromPointToLatLng(new google.maps.Point(mapRect[1][0], mapRect[0][1]), 7),
-                    mapRectSouthwest = fromPointToLatLng(new google.maps.Point(mapRect[0][0], mapRect[1][1]), 7),
-                    mapRectSoutheast = fromPointToLatLng(new google.maps.Point(mapRect[1][0], mapRect[1][1]), 7);
-
-                var mapRectPolygon = new google.maps.Polygon({
-                    paths: [
-                        mapRectNortheast,
-                        mapRectNorthwest,
-                        mapRectSouthwest,
-                        mapRectSoutheast,
-                        mapRectNortheast
-                    ],
-                    strokeColor: '#FFCC00',
-                    strokeOpacity: 0.8,
-                    strokeWeight: 2,
-                    fillColor: '#FF0000',
-                    fillOpacity: 0.35
-                  });
-
-                mapRectPolygon.setMap(map);
-
-            }
-        },
-        addMarker : function (location) {
-            var marker = new google.maps.Marker({
-                position: location,
-                map: map
-            });
-            return marker;
+            // https://tiles.guildwars2.com/1/1/zoom/{x}/{y}.jpg
+            return "https://tiles.guildwars2.com/1/1/"+zoom+"/"+normalizedCoord.x+"/"+normalizedCoord.y+".jpg";
         },
         getNormalizedCoord: function (coord, zoom) {
             var y = coord.y;
@@ -258,9 +163,144 @@ function fromPointToLatLng(point, max_zoom) {
         }
     };
 
+    var ui = {
+        mapRef: null,
+        mapNamesRequestResult: null,
+        mapFloorRequestResult: null,
+        initialize: function () {
+            var _this = this;
+
+            var urls = [
+                'https://api.guildwars2.com/v1/map_floor.json?continent_id=1&floor=2',
+                'https://api.guildwars2.com/v1/map_names.json'
+            ]
+
+            var setters = [
+                this.set_map_floor_request_result,
+                this.set_map_names_request_result
+            ]
+
+            var p = dataLoader.init(urls, setters);
+
+            this.mapRef = gmap.init();
+            mapNamesManager = new MapNames(_this.mapRef, gmap.getMaxZoom());
+
+            var stack = $.Callbacks();
+            stack.add([
+                this.get_explorables_zones_id,
+                this.get_datas_regions,
+                this.get_explorables_zones,
+                this.draw_explorables_zones,
+                this.update_map_name_manager,
+                $.proxy(mapNamesManager.update_display, mapNamesManager)
+            ])
+
+            $.when(p.done($.proxy(_this.apply_setter, _this)))
+                .then($.proxy(stack.fire, _this));
+
+            google.maps.event.addListener(_this.mapRef, 'zoom_changed', function(ev) {
+                mapNamesManager.update_display();
+            });
+
+
+            // map.overlayMapTypes.insertAt(0, new CoordMapType(new google.maps.Size(256, 256)));
+            // var p = new google.maps.Point(12839.8, 12483.8)
+            // var myLatLng = fromPointToLatLng(p, 7);
+            // var marker = this.addMarker(myLatLng, this.mapRef);
+            // this.addMarker(Snowden_Drifts_Southwest);
+            // this.addMarker(Snowden_Drifts_Southeast);
+            // this.addMarker(Snowden_Drifts_Northwest);
+            // this.addMarker(new google.maps.LatLng(-85, 180));
+
+        },
+        apply_setter: function(datas, setters) {
+            var len = setters.length;
+            for (var i=0; i<len; i++) {
+                if($.isFunction(setters[i])) {
+                    setters[i].call(this, datas[i]['responseJSON'])
+                }
+            }
+        },
+        set_map_names_request_result: function(datas) {
+            this.mapNamesRequestResult = datas;
+        },
+        set_map_floor_request_result: function(datas) {
+            this.mapFloorRequestResult = datas;
+        },
+        update_map_name_manager: function() {
+            mapNamesManager.set_infos_region(datas_regions);
+        },
+        get_explorables_zones_id: function() {
+            var mapNamesDatas = this.mapNamesRequestResult;
+            for (var i in mapNamesDatas) {
+                explorablesZonesId.push(mapNamesDatas[i]["id"]);
+            };
+        },
+        get_datas_regions: function() {
+            var mapFloorDatas = this.mapFloorRequestResult.regions;
+            for (var r in mapFloorDatas) {
+                // overlay = new nameOverlay(map_bounds, data1.mapFloorDatas[region].name, mapRef);
+                datas_regions.push([mapFloorDatas[r]['name'], mapFloorDatas[r]['label_coord']]);
+            }
+        },
+        get_explorables_zones: function() {
+            var mapFloorDatas = this.mapFloorRequestResult.regions;
+            for (var r in mapFloorDatas) {
+                for(var mapId in mapFloorDatas[r]['maps']) {
+                    //City are not present in map_names.json but we could find them in map_floor.json
+                    //because minLvl is 0 and maxLvl 80
+                    var minLvl = mapFloorDatas[r]['maps'][mapId]['min_level'];
+                    var maxLvl = mapFloorDatas[r]['maps'][mapId]['max_level'];
+                    if(explorablesZonesId.indexOf(mapId) != -1 || (minLvl == 0 && maxLvl == 80)) {
+                        explorablesZones.push([mapFloorDatas[r]['maps'][mapId]['name'], mapFloorDatas[r]['maps'][mapId]['continent_rect']])
+                    }
+                }
+            }
+        },
+        draw_explorables_zones: function() {
+            var map = this.mapRef,
+                len = explorablesZones.length;
+            // https://google-developers.appspot.com/maps/documentation/javascript/examples/polygon-simple
+            for(var i=0; i<len; i++) {
+                var mapRect = explorablesZones[i][1];
+
+                // [[17664, 11264],[21760, 13312]] -> [northwest, southeast]
+                var mapRectNorthwest = fromPointToLatLng(new google.maps.Point(mapRect[0][0], mapRect[0][1]), 7),
+                    mapRectNortheast = fromPointToLatLng(new google.maps.Point(mapRect[1][0], mapRect[0][1]), 7),
+                    mapRectSouthwest = fromPointToLatLng(new google.maps.Point(mapRect[0][0], mapRect[1][1]), 7),
+                    mapRectSoutheast = fromPointToLatLng(new google.maps.Point(mapRect[1][0], mapRect[1][1]), 7);
+
+                var mapRectPolygon = new google.maps.Polygon({
+                    paths: [
+                        mapRectNortheast,
+                        mapRectNorthwest,
+                        mapRectSouthwest,
+                        mapRectSoutheast,
+                        mapRectNortheast
+                    ],
+                    strokeColor: '#FFCC00',
+                    strokeOpacity: 0.8,
+                    strokeWeight: 2,
+                    fillColor: '#FF0000',
+                    fillOpacity: 0.35
+                  });
+
+                mapRectPolygon.setMap(map);
+
+            }
+        },
+        addMarker : function (location, map) {
+            var marker = new google.maps.Marker({
+                position: location,
+                map: map
+            });
+            return marker;
+        }
+    };
+
     context.ui = ui;
 
 })(front)
 
-$(document).ready(front.ui.initialize);
+$(document).ready($.proxy(front.ui.initialize, front.ui));
 //$(window).load(function() { front.ui.init() });
